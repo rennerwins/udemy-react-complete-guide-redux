68368f78bcf641539e1ba00d691f2813c73e9b00
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-12-13 22:58:01 -0800

    Outsourcing action types

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import React, { Component } from 'react';
 
+import * as actionTypes from '../../store/actions';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
@@ -65,12 +66,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
-    onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
-    onAddCounter: value => dispatch({ type: 'ADD', value }),
-    onSubstractCounter: value => dispatch({ type: 'SUBTRACT', value }),
-    onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
-    onDeleteResult: id => dispatch({ type: 'DELETE_RESULT', id })
+    onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+    onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+    onAddCounter: value => dispatch({ type: actionTypes.ADD, value }),
+    onSubstractCounter: value => dispatch({ type: actionTypes.SUBTRACT, value }),
+    onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
+    onDeleteResult: id => dispatch({ type: actionTypes.DELETE_RESULT, id })
   };
 };
 
