79d0c07f31528e09771bf53b2c2108a41a2a08dc
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-12-17 17:08:58 -0800

    Make switch case leaner

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,6 +5,11 @@ const initialState = {
   results: []
 };
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(result => action.id !== result.id);
+  return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
@@ -13,8 +18,7 @@ const reducer = (state = initialState, action) => {
       });
 
     case actionTypes.DELETE_RESULT:
-      const updatedArray = state.results.filter(result => action.id !== result.id);
-      return updateObject(state, { results: updatedArray });
+      return deleteResult(state, action);
 
     default:
       return state;
